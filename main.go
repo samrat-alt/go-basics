6663e5407b788cdffbfd8a62c33eacdd9131107a
samrat-alt <pandeysamrat855@gmail.com>
2024-03-19 20:42:40 +0545

    learned array and loops

--- main.go
+++ main.go
@@ -3,17 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "samrat pandey"
 
-	//fullname := "samrat pandey"
+	// var name string = "samrat pandey"
 
-	var age int
-	if name == "samrat pandey" {
-		age = 21
+	// fullname := "samrat pandey"
 
-	} else {
-		age = 0
+	// var age int
+	// if name == "samrat pandey" {
+	// 	age = 21
+
+	// } else {
+	// 	age = 0
+	// }
+
+	// fmt.Printf("\tmy name is %s.\nmy age is %d", name, age)
+
+	names := []string{
+		"samrat", "sagar",
 	}
 
-	fmt.Printf("\tmy name is %s.\nmy age is %d", name, age)
+	for i, value := range names {
+		fmt.Printf("%d. my name is %v\n", i+1, value)
+	}
+
 }
