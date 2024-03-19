0a9df3d07d549c513adc4e794ac5cae73bdd0652
samrat-alt <pandeysamrat855@gmail.com>
2024-03-19 20:26:48 +0545

    learned about variable and how to print

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "samrat pandey"
+
+	//fullname := "samrat pandey"
+
+	var age int
+	if name == "samrat pandey" {
+		age = 21
+
+	} else {
+		age = 0
+	}
+
+	fmt.Printf("\tmy name is %s.\nmy age is %d", name, age)
+}
